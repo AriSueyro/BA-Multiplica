32a9a7fac7df0991a2efeae9cb24d81a723be438
AriSueyro <asueyro@gmail.com>
2023-11-12 23:02:23 -0300

    Full Script

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 const open = document.getElementById("open")
 const modal_container = document.getElementById("modal_container")
+const modal_container2 = document.getElementById("modal_container2")
 const close = document.getElementById("close")
+const open1 = document.getElementById("open1")
+const close2 = document.getElementById("close2")
 
 open.addEventListener("click", () => {
     modal_container.classList.add("show")
@@ -10,3 +13,14 @@ open.addEventListener("click", () => {
 close.addEventListener("click", () => {
     modal_container.classList.remove("show")
 });
+
+open1.addEventListener("click", () => {
+    modal_container2.classList.add("show")
+});
+
+close2.addEventListener("click", () => {
+    modal_container2.classList.remove("show")
+});
+
+
+    
